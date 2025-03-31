277bc749cf6b66a17880be1580fa341c50fdfd63
GVD-OlerHealth <greg@olerhealth.com>
2025-03-31 16:02:52 -0400

    Add db_test route to connect to database

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from --your name-- in 3308'
+    return 'Hello World from Greg VanDyne in 3308'
 
 @app.route('/db_test')
 def db_test():
@@ -13,4 +13,4 @@ def db_test():
     conn = psycopg2.connect("postgresql://gregs_postgres_db_user:lGQhLBaitrrIdLVsGBuH10hzhrXHzw8G@dpg-cvlf428dl3ps738jql00-a/gregs_postgres_db")
     # Close
     conn.close()
-    return 'Database connection successful and closed!'
+    return 'Database connection successful!'
