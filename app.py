a41e275d708cc69a9301a2b91b23cadb7721f043
GVD-OlerHealth <greg@olerhealth.com>
2025-03-31 15:59:52 -0400

    Add db_test route to connect to database

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import psycopg2
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from --your name-- in 3308'
+
+@app.route('/db_test')
+def db_test():
+    # Connect to the database using the internal URL
+    conn = psycopg2.connect("postgresql://gregs_postgres_db_user:lGQhLBaitrrIdLVsGBuH10hzhrXHzw8G@dpg-cvlf428dl3ps738jql00-a/gregs_postgres_db")
+    # Close
+    conn.close()
+    return 'Database connection successful and closed!'
